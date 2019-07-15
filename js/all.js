76d8cc56c0a49f9d8b8725318b810d05af1790fa
Thiago Vieira Pereira <thiagov@ALFAMA.COM>
2019-07-15 18:53:34 -0300

    Conteúdo do cartão, efeito de girar em CSS e ajuste do container desdobrado

--- js/all.js
+++ js/all.js
@@ -1,26 +1,20 @@
 // Efeito de desdobramento
-$(function() {
+$(function () {
   var $container = $("#uc-container"),
     pfold = $("#uc-container").pfold({
       easing: "ease-in-out",
-      folds: 3,
-      folddirection: ["left", "bottom", "right"]
+      folds: 4,
+      folddirection: ["bottom", "right","bottom", "right"]
     });
 
   $container
     .find("h1.clickMe")
-    .on("click", function() {
+    .on("click", function () {
       pfold.unfold();
     })
     .end()
     .find("span.close")
-    .on("click", function() {
+    .on("click", function () {
       pfold.fold();
     });
-});
-
-// Efeito de giro no cartão
-var card = document.querySelector('.cartao');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('virado');
 });
